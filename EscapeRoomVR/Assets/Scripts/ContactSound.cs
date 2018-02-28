fc60adf2d37aefccfed412ff96afbd6e8cd06b11
Sauvage <b080s094@home.ku.edu>
2018-02-28 13:06:18 -0600

    Sound demo stuff

---
+++ EscapeRoomVR/Assets/Scripts/ContactSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContactSound : MonoBehaviour {
+
+	public AudioSource mySource;
+	public AudioClip floorSound;
+	public AudioClip pickUpSound;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Floor") {
+			mySource.clip = floorSound;
+		} else {
+			mySource.clip = pickUpSound;
+		}
+		mySource.Play ();
+	}
+}
