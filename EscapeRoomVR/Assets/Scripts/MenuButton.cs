fbe86984fe0b1c86d0d0a04c3417130683f40eac
Luciano Fasani <lucianofasani@gmail.com>
2018-03-21 23:26:18 -0500

    Menu System Working

---
+++ EscapeRoomVR/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour {
+
+    public enum ButtonMode {START, TUTORIAL, EXIT};
+
+    public ButtonMode currentMode;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void Action()
+    {
+        switch(currentMode)
+        {
+            case ButtonMode.START:
+                Debug.Log("START THE GAME");
+                SceneManager.LoadScene("lfasani1", LoadSceneMode.Single);
+                break;
+            case ButtonMode.TUTORIAL:
+                Debug.Log("START THE TUTORIAL");
+                break;
+            case ButtonMode.EXIT:
+                Debug.Log("EXIT THE GAME");
+                break;
+            default:
+                break;
+        }
+    }
+}
